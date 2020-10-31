d9dc446fe04bf84e8ef51d29103a361001c9750f
李凯林 <1399196696@qq.com>
2020-10-31 22:44:32 +0800

    第一次提交

--- assets/js/login.js
+++ assets/js/login.js
@@ -10,4 +10,23 @@ $(function() {
         $('.login-box').show()
         $('.reg-box').hide()
     })
+
+    // 自定义校验规则
+    var form = layui.form;
+    var layer = layui.layer
+    form.verify({
+        // 自定义密码的校验规则
+        pass: [
+            /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+        ],
+        // 自定义二次确认密码的单独校验规则
+        repwd: function(value) {
+            // 获取第一次密码的值
+            var pwd = $('.reg-box [name="password"]').val()
+            if (pwd !== value) {
+                return '您两次输入的密码不一致'
+            }
+        }
+    })
+
 })
